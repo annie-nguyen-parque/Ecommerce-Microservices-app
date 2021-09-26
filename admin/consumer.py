fe3c3846bee41e5f1b1ce9670525a1a9694c75af
Annie Nguyen <60783607+annie-nguyen-parque@users.noreply.github.com>
2021-09-26 12:44:55 +1000

    Add files via upload

---
+++ admin/consumer.py
@@ -0,0 +1,29 @@
+import pika, json, os, django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admin.settings')
+django.setup()
+from products.models import Product
+
+params = pika.URLParameters('amqps://zecduogl:ITzRUmrbZiZSJKW42rXFUvDt857RTNnj@beaver.rmq.cloudamqp.com/zecduogl')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='admin')
+
+def callback(ch, method, properties, body):
+    print('Received in admin')
+    data = json.loads(body)
+    print(data)
+    product = Product.objects.get(id=id)
+    product.likes = product.likes + 1
+    product.save()
+    print('Product likes increased!')
+
+channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
+
+print('Started consuming')
+
+channel.start_consuming()
+
+channel.close()
